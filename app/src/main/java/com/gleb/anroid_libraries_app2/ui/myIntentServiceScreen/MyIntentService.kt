fc6fc6de63dcc04081c2280a75c40f910b07fe9f
mzungu16 <gleb.s.smirnov@gmail.com>
2022-05-20 12:53:47 +0400

    Отнаследовался от Srvice

--- app/src/main/java/com/gleb/anroid_libraries_app2/ui/myIntentServiceScreen/MyIntentService.kt
+++ app/src/main/java/com/gleb/anroid_libraries_app2/ui/myIntentServiceScreen/MyIntentService.kt
@@ -1,20 +1,38 @@
 package com.gleb.anroid_libraries_app2.ui.myIntentServiceScreen
 
+import android.app.Service
 import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
-class MyIntentService {
-    private val intentList = mutableListOf<Intent>()
-    fun startMyService(intent: Intent) {
-        intentList.add(intent)
+class MyIntentService : Service() {
+    val ACTION_INTENT_SERVICE = "RESPONSE"
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.d("TAG", "Get - ${intent.getIntExtra("value", 0)}")
+        var value: String = ""
+        Observable.just(intent)
+            .subscribeOn(Schedulers.computation())
+            .map {
+                Thread.sleep(it.getIntExtra("timer", 0).toLong())
+                value = it.getIntExtra("value", 0).toString()
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                val responseIntent = Intent()
+                responseIntent.action = ACTION_INTENT_SERVICE
+                responseIntent.addCategory(Intent.CATEGORY_DEFAULT)
+                responseIntent.putExtra("resultValue2", value)
+                sendBroadcast(responseIntent)
+            }
+        return super.onStartCommand(intent, flags, startId)
     }
 
-    fun handleIntent(): Observable<String> {
-        return Observable.create { subscriber ->
-            for (i in intentList) {
-                Thread.sleep(i.getIntExtra("timer", 0).toLong())
-                subscriber.onNext(i.getIntExtra("value", 0).toString())
-            }
-        }
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
     }
 }
